21138ea0c4c22dca8453153e4478b91a2fdfef09
Diego Pedro <dpedro@HEXACTA.COM>
2019-09-02 15:53:36 -0300

    Change fold type, to avoid inference

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -6,5 +6,5 @@ import * as V from '../utils/validation'
 export const createUser = (req: Request, res: Response, next: NextFunction) => {
   const validation = res.locals.validation as V.Validation<Error[], User>
   const valueEither = validation.toEither()
-  return E.fold((errors) => res.status(400).json({ errors }), (user) => res.status(200).json({ user }))(valueEither)
+  return E.fold<Error[], User, Response>((errors) => res.status(400).json({ error: errors.map(error => error.message) }), (user) => res.status(200).json({ user }))(valueEither)
 }
